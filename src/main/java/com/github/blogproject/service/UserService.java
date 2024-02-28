d4a69b70418a49d591e2cba5db3e6f118d36eff7
sang hyeok Lee <saing0813@naver.com>
2024-02-28 20:54:41 +0900

    feat: create user Service and Controller

---
+++ src/main/java/com/github/blogproject/service/UserService.java
@@ -0,0 +1,23 @@
+package com.github.blogproject.service;
+
+import com.github.blogproject.domain.User;
+import com.github.blogproject.dto.AddUserRequest;
+import com.github.blogproject.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save (AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
